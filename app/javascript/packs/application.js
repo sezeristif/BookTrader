2e773b23238d8c8337bff03b6ebf43b698a6c8fd
sezeristif <sezeristif@gmail.com>
2020-12-09 11:47:42 +0300

    change datatable id

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,10 +10,4 @@ var jQuery = require('jquery')
 
 global.$ = global.jQuery = jQuery;
 window.$ = window.jQuery = jQuery;
-import $ from 'jquery';
 global.$ = jQuery;
-
-
-$(document).ready(function() {
-    $('#example').DataTable();
-});
