25e6f845bbb32a8f304cc25b8e480b93beccea9c
sezeristif <sezeristif@gmail.com>
2020-12-25 10:52:04 +0300

    fix pagination and delete unnecesary method

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def index
-    @visible_books = Book.paginate(page: params[:page], per_page: 6).where(public: true)
+    @visible_books = Book.where(public: true).paginate(page: params[:page], per_page: 6)
   end
 end
--- app/models/trade.rb
+++ app/models/trade.rb
@@ -3,8 +3,4 @@ class Trade < ApplicationRecord
   belongs_to :book
 
   enum trade_status: { pending: 0, traded: 1, rejected: -1 }
-
-  def offered_book
-    Book.find(offered_book_id)
-  end
 end
